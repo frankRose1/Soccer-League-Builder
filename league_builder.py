a593966c103ee1dba4fc9bd40afb6e288b0acead
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-10-10 13:35:17 -0400

    Add function to write each team and its players to a team.txt file

--- league_builder.py
+++ league_builder.py
@@ -5,6 +5,7 @@ inexperienced_players = []
 sharks = []
 raptors = []
 dragons = []
+number_of_teams = 3
 
 def read_csv():
     with open("soccer_players.csv") as csv_file:
@@ -13,7 +14,7 @@ def read_csv():
         counter = 0
         for row in rows:
             if counter == 0:
-                print(', '.join(row))
+                #print(', '.join(row))
                 counter += 1
             elif row[2] == 'YES':
                 experienced_players.append(row)
@@ -26,23 +27,46 @@ def read_csv():
 
 
 def create_teams():
-    print(len(experienced_players))
-    print('-'* 40)
-    print(len(inexperienced_players))
+    """Each team will get 6 players, with an even number of experienced players on each team.
+    """
+    total_players = len(experienced_players) + len(inexperienced_players)
+    players_per_team = total_players / number_of_teams
+    i = 0
+    while i < players_per_team / 2:
+        add_players_to_team(raptors)
+        add_players_to_team(sharks)
+        add_players_to_team(dragons)
+        i += 1
+
+    create_roster_file()
 
 
-# TODO 
-"""Create variables and programming logic to divide the 18 players into three teams: Sharks, Dragons and Raptors. 
-Make sure the teams have the same number of players on them, and that the experience players are divided equally across the three teams."""
+def add_players_to_team(team):
+    team.append(experienced_players.pop())
+    team.append(inexperienced_players.pop())
 
-#TODO
-"""Create a text file named teams.txt that includes the name of a team, followed by the players on that team. 
-List all three teams and their players."""
 
-#TODO
-"""In the list of teams include the team name on one line, followed by a separate line for each player. Include the player's name, 
-whether the player has experience playing soccer, and the player's guardian names. 
-Separate each bit of player information by a comma."""
+def create_roster_file():
+    """Create a text file named teams.txt that includes the name of a team, followed by the players on that team. 
+    List all three teams and their players. Provide the player name, parent/gaurdian name, and if they are experienced"""
+    #loop over each team
+
+    #write the names an teams in that file
+    file = open('teams.txt', 'a')
+    file.write(file_content(raptors, 'raptors'))
+    file.write(file_content(sharks, 'sharks'))
+    file.write(file_content(dragons, 'dragons'))
+
+
+def file_content(team, team_name):
+    string = ''
+    string += '{} \n'.format(team_name.upper())
+    string += '-'*20
+    string+= '\n'
+    for player in team:
+        string += '{}, {}, {} \n'.format(player[0], player[2], player[3])
+    string += '\n \n \n'
+    return string
 
 
 if __name__ == '__main__':
